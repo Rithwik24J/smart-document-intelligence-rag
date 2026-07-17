1ecc1b96ee6435d3f16f3535eca53151bfe4050f
Mayachari Rithwik <rithwikmayachari@gmail.com>
2026-07-17 19:37:06 +0530

    Add project screenshots

--- test_groq.py
+++
@@ -1,7 +0,0 @@
-from llm import load_llm
-
-llm = load_llm()
-
-response = llm.invoke("Say hello.")
-
-print(response.content)
--- test_llm.py
+++
@@ -1,7 +0,0 @@
-from llm import load_llm
-
-llm = load_llm()
-
-response = llm.invoke("Say hello in one sentence.")
-
-print(response.content)
